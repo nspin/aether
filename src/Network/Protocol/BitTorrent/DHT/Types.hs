3354f96c745048336577454a3639c014154abdc4
Nick Spinale <spinalen@carleton.edu>
2015-03-30 09:02:02 -0500

    added first file

---
+++ src/Network/Protocol/BitTorrent/DHT/Types.hs
@@ -0,0 +1,33 @@
+module Network.Protocol.BitTorrent.DHT.Types
+    (
+    ) where
+
+import           Data.Concurrent.STM
+import           Data.Aencode
+import           Data.Acid
+import qualified Data.ByteString as B
+import           Data.Word.Play
+
+data Message = undefined
+
+instance ToAeson Message where
+
+instance FromAeson Message where
+
+data Node a = Node
+    { location :: Word160
+    , port     :: Word16
+    , addr     :: a
+    }
+
+data Peer a = Peer
+    { node :: Node
+    , last :: UnixTime
+    , answered :: Bool
+    }
+
+type RespState = M.Map B.ByteString TMVar
+
+-- Acidic
+type Buckets = [[Node]]
+
