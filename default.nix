64f8a6d1e087c4c68e17edc08d6301f6225d4838
Nick Spinale <spinalen@carleton.edu>
2015-10-04 18:53:35 -0500

    organized and added nix expression

---
+++ default.nix
@@ -0,0 +1,15 @@
+{ mkDerivation, aencode, attoparsec, base, bigword, bytestring
+, containers, lens, network, stdenv, stm, time
+}:
+mkDerivation {
+  pname = "aether";
+  version = "0.1.0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    aencode attoparsec base bigword bytestring containers lens network
+    stm time
+  ];
+  homepage = "https://github.com/nickspinale/aether.git";
+  description = "BitTorrent mainline DHT";
+  license = stdenv.lib.licenses.mit;
+}
